044febc7d95c40511dd51377f52221f8baee2e07
Gustavo Sena <GamingUnzado12@gmail.com>
2023-10-26 13:57:31 -0300

    Adding ejs views to make the project more readable

--- app.js
+++ app.js
@@ -1,29 +1,19 @@
 const express = require("express");
-const { fetchProductDetails } = require("./productDetails");
-
+const path = require("path");
 const app = express();
+
+// Set up EJS as the view engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.json());
 
 // Home route to describe the API usage
 app.get("/", (req, res) => {
-  res.send(`
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Product Details Extractor API</title>
-    </head>
-    <body>
-        <h1>Product Details Extractor API</h1>
-        <p>To extract product details from a webpage, make a POST request to the following URL:</p>
-        <code>/fetch-product-details</code>
-        <p>Include a request body with the URL from which you want to extract product details, like so:</p>
-        <code>{ "url": "https://www.example.com/product" }</code>
-    </body>
-    </html>
-  `);
+  res.render("index");
 });
 
 app.post("/fetch-product-details", async (req, res) => {
