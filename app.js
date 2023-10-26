86e7c3268e0c09b5f25c3c3a65eaddba27d3f773
Gustavo Sena <GamingUnzado12@gmail.com>
2023-10-26 19:46:54 -0300

    Changing main file in package.json

--- app.js
+++ app.js
@@ -40,5 +40,5 @@ app.post("/fetch-product-details", async (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on http://0.0.0.0:${PORT}`);
+  console.log(`Server is running on http://AWSServiceURL:${PORT}`);
 });
